b2d58b60dc1cdaeb79ed37241705e8c3677a9f19
Sangharsh Boudhh <sboudhh@gmail.com>
2024-07-11 17:10:38 +0530

    Organize directories

--- ci/concourse/run.sh
+++ ci/concourse/run.sh
@@ -3,7 +3,7 @@
 # docker compose up -d
 
 fly -t tutorial login -c http://localhost:8080 -u test -p test
-fly -t tutorial set-pipeline -p inventory -c ../../inventory/build.yml
+fly -t tutorial set-pipeline -p inventory -c ../../apps/inventory/build.yml
 # pipelines are paused when first created
 fly -t tutorial unpause-pipeline -p inventory
 # trigger the job and watch it run to completion
