08690b7bc942195c85bf50da98a62c2f5d951e21
Sangharsh Boudhh <sboudhh@gmail.com>
2024-07-24 12:14:26 +0530

    Standardize names for k8s resources

--- apps/inventory/k8s/run.sh
+++ apps/inventory/k8s/run.sh
@@ -5,10 +5,10 @@ minikube start
 
 # TODO: Consider replacing helm with k8s yamls for postgres
 # May need pvc, deployment, secret, service
-helm install inventory-db bitnami/postgresql -f k8s/postgres-values.yaml --version 15.5.16
+helm install inventory bitnami/postgresql -f k8s/postgres-values.yaml --version 15.5.16
 sleep 20
-kubectl create configmap db-init-sql --from-file=db-init.sql=./db-init.sql
-kubectl apply -f k8s/postgres-init-job.yaml
+kubectl create configmap inventory-init-data --from-file=db-init.sql=./db-init.sql
+kubectl apply -f k8s/init-job.yaml
 # Point docker client to dockerd inside minikube
 eval $(minikube -p minikube docker-env)
 docker build -t inventory:latest .
@@ -21,8 +21,8 @@ minikube service ${SERVICE_NAME} --url
 
 # Uninstall and clean up
 kubectl delete deploy inventory
-kubectl delete service inventory-service
-kubectl delete job postgres-init-job
-kubectl delete configmap db-init-sql
-helm uninstall inventory-db
-kubectl delete pvc data-inventory-db-postgresql-0
+kubectl delete service inventory
+kubectl delete job inventory-init-job
+kubectl delete configmap inventory-init-data
+helm uninstall inventory
+kubectl delete pvc data-inventory-postgresql-0
